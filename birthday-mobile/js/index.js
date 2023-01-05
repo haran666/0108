581bb6695da9fa3c4c51c0d163919a5da386adaa
haran666 <884966517@qq.com>
2023-01-05 21:00:46 +0800

    2

--- birthday-mobile/js/index.js
+++ birthday-mobile/js/index.js
@@ -2,13 +2,13 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="123" &&  pwd=="123"){
+    if(userName=="李雨晴" &&  pwd=="0108"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
 			setTimeout(function(){location.href="1.html";},2000);
 		}
 	else{
-		alert("Wrong Password");
+		alert("密码是我最爱的小李子的生日哦!（4位数）");
 	}
 });
